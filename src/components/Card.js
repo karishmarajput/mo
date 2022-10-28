1217133b3e0dc651ac315ec52eee4d86dc2d2c03
karishmarajput <84656423+karishmarajput@users.noreply.github.com>
2022-10-28 23:55:19 +0530

    3 files modified

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import './style/card.css';
-import './data/cardData.json';
+import data from './data/cardData.json';
+console.log(data);
+function RenderPost(post) {
+  console.log(post);
+  return (
+    <div className="cardDiv" key={post.id} style={{ width: '100%' }}>
+      <div className="cardHeading">{post.name}</div>
+      <div className="cardImg">
+        <img src={post.src} />
+      </div>
+      <div className="cardDetails">
+        <div className="cardLike"></div>
+        <div className="cardShare">
+          <i class="fa-solid fa-share"></i>
+        </div>
+        <div className="cardDonate"></div>
+      </div>
+    </div>
+  );
+}
+
 export default function Card() {
-  const comment = comments.map((comm) => {
+  const post = data.post.map((post) => {
     return (
-      <div className="cardDiv" key={comm.id} style={{ width: '100%' }}>
-        <div className="cardHeading">{comm.name}</div>
-        <div className="cardImg">
-          <img src={comm.src} />
-        </div>
-        <div className="cardDetails">
-          <div className="cardLike"></div>
-          <div className="cardShare">
-            <i class="fa-solid fa-share"></i>
-          </div>
-          <div className="cardDonate"></div>
-        </div>
+      <div key={post.id} className="cards">
+        <RenderPost post={post} />
       </div>
     );
   });
   return (
     <div className="container">
-      <div className="row">
-        {comment}
-        {/* <CommentForm dishId={dishId} addComment={addComment} /> */}
-      </div>
+      <div className="row">{post}</div>
     </div>
   );
 }
