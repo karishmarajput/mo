abaa07df181b08a20f7fa7a0ea0384bb5231ee26
karishmarajput <84656423+karishmarajput@users.noreply.github.com>
2022-10-28 23:05:46 +0530

    1 file modified

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,14 @@
 import React from 'react';
 export default function Motivation() {
-  return(
+  return (
     <div>
-      <div>
+      <div className="cardHeading"> Heading</div>
+      <div className="cardImg"></div>
+      <div className="cardDetails">
+        <div className="cardLike"></div>
+        <div className="cardShare"></div>
+        <div className="cardDonate"></div>
+      </div>
     </div>
   );
 }
