cebade675eb77bcbbdf78ba624757e88db27ba2e
karishmarajput <84656423+karishmarajput@users.noreply.github.com>
2022-10-28 23:42:14 +0530

    2 files modified, 1 added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './style/card.css';
-export default function Motivation() {
-  return (
-    <div className="cardDiv">
-      <div className="cardHeading"> Heading</div>
-      <div className="cardImg"></div>
-      <div className="cardDetails">
-        <div className="cardLike"></div>
-        <div className="cardShare">
-          <i class="fa-solid fa-share"></i>
+import './data/cardData.json';
+export default function Card() {
+  const comment = comments.map((comm) => {
+    return (
+      <div className="cardDiv" key={comm.id} style={{ width: '100%' }}>
+        <div className="cardHeading">{comm.name}</div>
+        <div className="cardImg">
+          <img src={comm.src} />
         </div>
-        <div className="cardDonate"></div>
+        <div className="cardDetails">
+          <div className="cardLike"></div>
+          <div className="cardShare">
+            <i class="fa-solid fa-share"></i>
+          </div>
+          <div className="cardDonate"></div>
+        </div>
+      </div>
+    );
+  });
+  return (
+    <div className="container">
+      <div className="row">
+        {comment}
+        {/* <CommentForm dishId={dishId} addComment={addComment} /> */}
       </div>
     </div>
   );
